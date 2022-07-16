4b2d5fc65ebb1f505b4b8aa54168696dc6c4d05a
kajela <titoyut56@gmail.com>
2022-07-16 11:05:09 -0400

    add the initial files

---
+++ src/main/java/org/example/Ass.java
@@ -0,0 +1,84 @@
+//package org.example;
+//
+//class Ass {
+//    public static void Square(){
+//        int size = 5;
+//    }
+//    for (int row = 0; row < size;row++){
+//        System.out.print("# ");
+//        for(int col = 0;col < size;col++){
+//            System.out.print("# ");
+//        }
+//        System.out.println();
+//    }
+//}
+//    public static void check_box(){
+//    int size = 7;
+//    }
+//    for(int row = 1;row < size;row ++){
+//        if (row % 2 == 0){System.out.print(" # ");
+//        }else{
+//            System.out.print("# ");
+//        }
+//        for (int col = 1;col < size;col++){
+//            System.out.print("# ");
+//        }
+//        System.out.println();
+//    }
+//        }
+//}public static void times_table(){
+//     times tableint size = 12;System.out.println();
+//         for (int i = 1;i < size + 1;i++){
+//             System.out.printf(" %d | ", i);
+//             for (int n = 1;n < size + 1;n++){
+//                 int total = i*n;if (total < 10)
+//                     System.out.printf(" %d ", total);
+//                 else if (total < 100)System.out.printf(" %d ", total);
+//                 elseSystem.out.printf(" %d", total);
+//             }
+//             System.out.println("\n");
+//         }
+//}public static void triangle_a(){
+//    for (int i = 1;i < 10;i++){
+//        System.out.print("*");
+//        for (int n = 1;n < i;n++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//    }
+//    System.out.println();
+//}
+//public static void triangle_b(){
+//    for (int i = 10;i > 1; i--){
+//        System.out.print("*");
+//        for (int n = i;n > 1;n--){
+//        System.out.print("*");
+//        }
+//        System.out.println();}System.out.println();
+//}
+//public static void triangle_c(){
+//    int row = 10;for (int i = row;i > 1;i--){
+//        for (int n = 0;n < row -i;n++){
+//            System.out.print(" ");
+//        }
+//        for (int x = i;x > 1;x--){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//    }
+//}public static void triangle_d(){
+//    int
+//        }
+//        rows = 10;for(int i = 0; i < rows;i++){
+//            for
+//        }
+//        (int n = 0; n < rows-i;n++){
+//            System.out.print(" ");
+//        }
+//        for (int x = 0;x < i;x++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//        }
+//        }
+//        }
